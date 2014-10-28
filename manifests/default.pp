6270ccd9cf3c5fdb0c3e5336fe028abcbcba6dd4
jdgwartney <davidg@boundary.com>
2014-10-28 07:12:05 -0700

    Refactor for single RabbitMQ instance

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,46 +5,26 @@ exec { "apt-get update":
 
 # Install RabbitMQ
 package { 'rabbitmq-server': ensure  => 'latest' }
-package { 'git': ensure  => 'latest' }
+package { 'wget': ensure  => 'latest' }
+package { 'curl': ensure  => 'latest' }
 
 # Start the RabbitMQ service
-#service { "rabbitmq-server":
-#  ensure  => "running",
-#  require => Package["rabbitmq-server"],
-#}
+service { "rabbitmq-server":
+  ensure  => "running",
+  require => Package["rabbitmq-server"],
+}
 
-
-file { 'cookie':
-path => '/var/lib/rabbitmq/.erlang.cookie',
-mode => 0400,
-owner => rabbitmq,
-group => rabbitmq,
-source => '/vagrant/cookie',
-require => Package[rabbitmq-server]
+#
+exec { "install rabbitmq java client":
+  command => "install-rabbitmq-client.sh",
+  path => "/vagrant",
 }
 
 file { 'profile':
-path => '/home/vagrant/.bash_profile'
+path => '/home/vagrant/.bash_profile',
 mode => 0400,
 owner => vagrant,
 group => vagrant,
 source => '/vagrant/bash_profile',
 require => Package[rabbitmq-server]
 }
-
-
-
-# Cluster Support
-#
-# Set up /etc/hosts so that all servers can communicate
-#
-host { 'rabbit1':
-    ip => '192.168.33.11',
-}
-host { 'rabbit2':
-    ip => '192.168.33.12',
-}
-host { 'rabbit3':
-    ip => '192.168.33.13',
-}
-
